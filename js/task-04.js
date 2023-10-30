f3a41edafb1201095409f1f39841af05cdab7a23
syveer <hanganalexandra@yahoo.com>
2023-10-30 23:25:33 +0200

    hw3
    hw3

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,23 @@
-let counterValue = 0;
-const valueElement = document.getElementById("value");
-const incrementButton = document.querySelector("[data-action='increment']");
-const decrementButton = document.querySelector("[data-action='decrement']");
-const valueElement = document.getElementById("value");
-const incrementButton = document.querySelector("[data-action='increment']");
-const decrementButton = document.querySelector("[data-action='decrement']");
-incrementButton.addEventListener("click", () => {
-  counterValue += 1;
-  valueElement.textContent = counterValue;
-});
-decrementButton.addEventListener("click", () => {
-  counterValue -= 1;
-  valueElement.textContent = counterValue;
-});
+ let counterValue = 0;
+
+  
+  const decrementButton = document.querySelector('[data-action="decrement"]');
+  const incrementButton = document.querySelector('[data-action="increment"]');
+  const valueSpan = document.getElementById('value');
+
+  
+  decrementButton.addEventListener('click', () => {
+    counterValue -= 1; 
+    updateCounter(); 
+  });
+
+  
+  incrementButton.addEventListener('click', () => {
+    counterValue += 1; 
+    updateCounter(); 
+  });
+
+  
+  function updateCounter() {
+    valueSpan.textContent = counterValue;
+  }
